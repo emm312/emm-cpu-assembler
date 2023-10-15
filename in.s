facce51102ad62a15bba1e4f546a639d89235c51
emm312 <emmtbm55@gmail.com>
2023-10-15 16:34:19 +1100

    stuff

--- in.s
+++ in.s
@@ -1,20 +1,7 @@
-mov r5, 249
-cal .display_num
+mov r1, 7
+mov r2, 24
+
+mul r1, r1, r1
+mul r2, r2, r2
+add r1, r2, r1
 hlt
-
-.display_num
-    mov r3, 0
-.DISPLAY_NUM_loop
-    mod r4, r5, 10
-    add r4, r4, 48
-    pst r3, r4
-    div r5, r5, 10
-    add r3, r3, 1
-    cmp 0, r5
-    jgr .DISPLAY_NUM_loop
-    ret
-
-.reverse_arr
-    mov r6, 0
-    ret
-    
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 use std::{io::Write, process::exit};
 
-use assembler::{assembler::remove_labels, grammar};
+use cool_cpu_assembler::{assembler::remove_labels, grammar};
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[arg()]
+    input: String,
+    #[arg(short, default_value_t=String::from("out.bin"))]
+    output: String
+}
+
 
 fn main() {
-    let input = std::fs::read_to_string("in.s").unwrap();
+    let args = Args::parse();
+    let input = std::fs::read_to_string(args.input).unwrap();
     let ast = remove_labels(grammar::AsmParser::new().parse(&input).unwrap_or_else(|e| {
         println!("{e}");
         exit(-1);
     }));
-    std::fs::File::create("out.bin")
+    let mut output = String::new();
+    for word in cool_cpu_assembler::assembler::assemble(ast) {
+        output.push_str(&format!("{:02x}\n", word));
+    }
+    std::fs::File::create(args.output)
         .unwrap()
-        .write_all(&assembler::assembler::assemble(ast))
+        .write_all(&output.as_bytes())
         .unwrap();
 }
